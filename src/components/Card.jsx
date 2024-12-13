6f6e90c30f2be32d996a0fba4d8da21be175ddef
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-13 13:53:06 +0100

    posts data, render posts data in cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import Badge from "./Badge";
 import style from "./Card.module.css";
 
-function Card({ titolo, contenuto, immagine }) {
+function Card({
+    title,
+    content,
+    image = "https://picsum.photos/200/300",
+}) {
     // card states
     const [modal, setModal] = useState(false);
 
@@ -18,23 +22,29 @@ function Card({ titolo, contenuto, immagine }) {
     return (
         <div
             onClick={handleCardClick}
-            className={`${style.cardRatio} bg-white flex flex-col rounded-xl overflow-hidden cursor-pointer shadow-lg shadow-stone-500  hover:scale-110 hover:rotate-6 hover:shadow-xl hover:shadow-zinc-700 transition-all ${
+            className={`${
+                style.cardRatio
+            } bg-white flex flex-col rounded-xl overflow-hidden cursor-pointer shadow-lg shadow-stone-500  hover:scale-110 hover:rotate-6 hover:shadow-xl hover:shadow-zinc-700 transition-all ${
                 modal && modalClass
             }`}
         >
             <div className={`h-3/5 bg-red-300`}>
                 <img
-                    className="h-full object-cover"
-                    src={immagine}
-                    alt={titolo}
+                    className="h-full w-full object-cover"
+                    src={image}
+                    alt={title}
                     loading="lazy"
                 />
             </div>
-            <div className={`grow px-3 py-2 flex flex-col justify-between items-start gap-1`}>
+            <div
+                className={`grow px-3 py-2 flex flex-col justify-between items-start gap-1`}
+            >
                 <div>
-                    <h2 className="text-lg font-semibold line-clamp-1">{titolo}</h2>
+                    <h2 className="text-lg font-semibold line-clamp-1">
+                        {title}
+                    </h2>
                     <p className="my-1 leading-5 hyphens-auto line-clamp-2">
-                        {contenuto}
+                        {content}
                     </p>
                 </div>
                 <Badge />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import Card from "./Card";
 import { foods } from "../data/foods";
+import posts from "../data/posts";
 
 function Main({ titolo }) {
     return (
@@ -8,12 +9,12 @@ function Main({ titolo }) {
                 {titolo}
             </h1>
             <div className="w-3/4 xl:w-3/5 mx-auto grid auto-rows-fr grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {foods.map((food) => (
+                {posts.map((post) => (
                     <Card
-                        key={food.id}
-                        immagine={food.immagine}
-                        titolo={food.titolo}
-                        contenuto={food.contenuto}
+                        key={post.id}
+                        image={post.image}
+                        title={post.title}
+                        content={post.content}
                     ></Card>
                 ))}
             </div>
