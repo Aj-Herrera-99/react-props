28f9abfd6f04deceeed9b3d8a16a6948f03ea4a5
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-13 15:20:02 +0100

    responsive sidebar (no reactive)

--- src/components/AsideBar.jsx
+++ src/components/AsideBar.jsx
@@ -1,9 +1,13 @@
 function AsideBar({ tags }) {
     return (
-        <aside className="bg-stone-700 text-white w-[40vw] p-5">
-            <ul>
+        <aside className="bg-stone-600 text-white w-0 md:w-auto relative">
+            <div className="cursor-pointer text-4xl absolute top-0 right-[-50px] md:hidden">
+                <i className="fa-solid fa-bars text-stone-600"></i>
+            </div>
+            <ul className="text-2xl hidden  md:flex flex-col gap-3 p-5">
                 {tags.map((tag, index) => (
-                    <li key={index}>{tag}</li>
+                    <li key={index}
+                    className="pr-32">{tag}</li>
                 ))}
             </ul>
         </aside>
