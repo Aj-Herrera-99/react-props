49f85aa38e13d63724c0b208fd8d8995bea92d29
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-13 14:19:09 +0100

    filtro in base al valore di published

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,15 +9,17 @@ function Main({ titolo }) {
                 {titolo}
             </h1>
             <div className="w-3/4 xl:w-3/5 mx-auto grid auto-rows-fr grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {posts.map((post) => (
-                    <Card
-                        key={post.id}
-                        image={post.image}
-                        title={post.title}
-                        content={post.content}
-                        tags={post.tags}
-                    ></Card>
-                ))}
+                {posts
+                    .filter((post) => post.published == true)
+                    .map((post) => (
+                        <Card
+                            key={post.id}
+                            image={post.image}
+                            title={post.title}
+                            content={post.content}
+                            tags={post.tags}
+                        ></Card>
+                    ))}
             </div>
         </main>
     );
